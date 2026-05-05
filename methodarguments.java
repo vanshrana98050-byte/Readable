b1240f54bb86b759c4816bd987a4cdbd6c556a6a
Vansh Rana <vanshrana98050@gmail.com>
2026-05-06 00:39:54 +0530

    Add files via upload

---
+++ methodarguments.java
@@ -0,0 +1,9 @@
+public class methodarguments{
+    public static void main(String[] args) {
+printsomething("My Name Is Vansh Rana");        
+    }
+
+    public static void printsomething(String name){
+        System.out.println(name);
+    }
+}
