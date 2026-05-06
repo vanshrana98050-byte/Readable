487e6531a9ed42d1bdc261c27c96b696186c138b
Vansh Rana <vanshrana98050@gmail.com>
2026-05-06 22:41:32 +0530

    Add files via upload

---
+++ argument2.java
@@ -0,0 +1,16 @@
+public class argument2{
+public static void main(String[] args) {
+    printsomething(18);
+    printsomething("Vansh Rana");
+}
+
+public static void printsomething(int age){
+    System.out.println("my age is "+age);
+
+   
+}
+public static void printsomething(String name){
+
+    System.out.println("my name is"+name);
+}
+}
