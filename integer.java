a755435e7fb0661d09f5930afde4283a58abcd06
Vansh Rana <vanshrana98050@gmail.com>
2026-05-11 23:30:07 +0530

    Add files via upload

---
+++ integer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class integer {
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<4; i++){
+            System.out.println("Enter An Integer");
+        
+        int n =sc.nextInt();
+        System.out.println(n);
+
+    }
+}
+}
