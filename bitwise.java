576109892b68ffb1163ee0a221f83b5346767005
Vansh Rana <vanshrana98050@gmail.com>
2026-05-11 09:40:23 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,13 @@
+public class bitwise {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+
+        System.out.println("a & b = " + (a & b));
+        System.out.println("a | b = " + (a | b));
+        System.out.println("a ^ b = " + (a ^ b));
+        System.out.println("~a = " + (~a));
+        System.out.println("a << 1 = " + (a << 1));
+        System.out.println("a >> 1 = " + (a >> 1));
+        System.out.println("a >>> 1 = " + (a >>> 1));
+    }
+}
