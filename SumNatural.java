a9812878355e2bd15d10b4d84ebfa17f38b5791a
Vansh Rana <vanshrana98050@gmail.com>
2026-05-13 23:37:18 +0530

    Add files via upload

---
+++ SumNatural.java
@@ -0,0 +1,11 @@
+public class SumNatural{
+    public static void main(String[]args){
+        int n=20;
+        int sum=0;
+
+        for(int i=0; i<21; i++) {
+            sum = sum+i;
+        }
+        System.out.println("the sum is "+sum);
+    }
+}
