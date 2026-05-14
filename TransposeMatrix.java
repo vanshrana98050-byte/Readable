a08319a1fdb389f3c32317c202da1f0c0f25ec51
Vansh Rana <vanshrana98050@gmail.com>
2026-05-14 22:44:36 +0530

    Add files via upload

---
+++ TransposeMatrix.java
@@ -0,0 +1,19 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+
+        int arr[][] = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for(int i = 0; i < 3; i++) {
+
+            for(int j = 0; j < 2; j++) {
+
+                System.out.print(arr[j][i] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
