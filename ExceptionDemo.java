d530ac9187ff6cea9478aac98c7db154a479f8b2
Vansh Rana <vanshrana98050@gmail.com>
2026-05-15 22:58:48 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,17 @@
+public class ExceptionDemo {
+    public static void main(String[] args) {
+
+        try {
+
+            int a = 10 / 0;
+
+            System.out.println(a);
+
+        } catch(Exception e) {
+
+            System.out.println("Cannot divide by zero");
+        }
+
+        System.out.println("Program End");
+    }
+}
