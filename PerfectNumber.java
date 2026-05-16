31d55bbdd535694cd2a0ee222b50cd56721f7540
Vansh Rana <vanshrana98050@gmail.com>
2026-05-16 23:19:39 +0530

    Add files via upload

---
+++ PerfectNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i < num; i++) {
+
+            if(num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if(sum == num) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not Perfect Number");
+        }
+    }
+}
