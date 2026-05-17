f2aa0510fce19293a83ff5aae6766741bc9cb105
Vansh Rana <vanshrana98050@gmail.com>
2026-05-17 21:16:03 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,12 @@
+public class CircleArea {
+
+    static double area(double radius) {
+
+        return 3.14 * radius * radius;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Area = " + area(5));
+    }
+}
