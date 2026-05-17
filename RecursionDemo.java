ac06422fd69035d2b541f8f26de0398720d3bf94
Vansh Rana <vanshrana98050@gmail.com>
2026-05-17 21:00:37 +0530

    Add files via upload

---
+++ RecursionDemo.java
@@ -0,0 +1,18 @@
+public class RecursionDemo {
+
+    static void print(int n) {
+
+        if(n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+
+        print(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        print(5);
+    }
+}
