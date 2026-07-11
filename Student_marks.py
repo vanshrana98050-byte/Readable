3db45cd0ae9f5eafbe91d03ecf064dce75077c29
Vansh Rana <vanshrana98050@gmail.com>
2026-07-11 23:29:49 +0530

    Add files via upload

---
+++ Student_marks.py
@@ -0,0 +1,8 @@
+marks=int(input("enter the marks of student"))
+
+if (marks>70):
+    print("student get A grade!")
+if (marks>=50):
+    print("Student get B grade!")
+else:
+    print("Student is fail!")        
