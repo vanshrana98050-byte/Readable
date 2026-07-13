d8f6f44238d34a99a2512adcdfccc298404060fa
Vansh Rana <vanshrana98050@gmail.com>
2026-07-13 22:25:52 +0530

    Add files via upload

---
+++ greater_of_two.py
@@ -0,0 +1,14 @@
+a=int(input("enter the number 1: "))
+c=int(input("enter the number 2: "))
+b=int(input("enter the number 3: "))
+
+
+
+if (a>c and a>b):
+    print("1 num is greater: ")
+elif (c>a and c>b):
+    print("2 num is greater")
+else:
+    print("3 num is greater: ")        
+
+
