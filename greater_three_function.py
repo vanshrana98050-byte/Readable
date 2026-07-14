f388e7455468297916e5adb9e7211402ed8c4406
Vansh Rana <vanshrana98050@gmail.com>
2026-07-15 01:00:50 +0530

    Add files via upload

---
+++ greater_three_function.py
@@ -0,0 +1,15 @@
+def grt():
+    a=int(input("enter first number"))    
+    b=int(input("enter second number"))    
+    c=int(input("enter third number"))    
+    if(a>b)and(a>c):
+        print("First number  is greater")
+
+    elif(b>a)and(b>c):
+        print("Second number is greater")
+
+    else:
+        print("Third number is greater")
+
+
+grt()            
