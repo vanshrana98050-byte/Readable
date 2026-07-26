53e329a8c1fd93ce62b630df3fc4f2e97c8d677b
Vansh Rana <vanshrana98050@gmail.com>
2026-07-26 21:33:02 +0530

    Add files via upload

---
+++ N_Queens_Problem.py
@@ -0,0 +1,35 @@
+def solveNQueens(n):
+    result = []
+
+    cols = set()
+    diag1 = set()
+    diag2 = set()
+
+    board = [["."] * n for _ in range(n)]
+
+    def backtrack(r):
+        if r == n:
+            result.append(["".join(row) for row in board])
+            return
+
+        for c in range(n):
+            if c in cols or (r - c) in diag1 or (r + c) in diag2:
+                continue
+
+            cols.add(c)
+            diag1.add(r - c)
+            diag2.add(r + c)
+
+            board[r][c] = "Q"
+
+            backtrack(r + 1)
+
+            board[r][c] = "."
+            cols.remove(c)
+            diag1.remove(r - c)
+            diag2.remove(r + c)
+
+    backtrack(0)
+    return result
+
+print(solveNQueens(4))
