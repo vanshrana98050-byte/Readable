0a5cb3ae760bbdadfbcc7a5c7ea7d691b77324d1
Vansh Rana <vanshrana98050@gmail.com>
2026-07-28 22:27:13 +0530

    Add files via upload

---
+++ .Generate_Parentheses.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+result = []
+
+def backtrack(cur, open_count, close_count):
+
+    if len(cur) == 2 * n:
+        result.append(cur)
+        return
+
+    if open_count < n:
+        backtrack(cur + "(", open_count + 1, close_count)
+
+    if close_count < open_count:
+        backtrack(cur + ")", open_count, close_count + 1)
+
+backtrack("", 0, 0)
+
+for s in result:
+    print(s)
